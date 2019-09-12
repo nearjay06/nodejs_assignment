7b2a8454be50d2cef57eeac8c304529b5bcd783c
nearjay06 <nearjoan2018@gmail.com>
2019-09-12 11:05:04 +0300

    create babelrc,packagejson,packagelockjason files, javascript files and src and public folders and index.html and css files

---
+++ src/app/Obj.js
@@ -0,0 +1,11 @@
+const Person={
+    Name:'james',
+    Born year:'1967',
+    Age: console.log(calculate_age(new Date(1982, 11, 4))); 
+    35
+    // function to calculate the age(…use
+    // date object)
+    }
+
+
+console.log(Person);
